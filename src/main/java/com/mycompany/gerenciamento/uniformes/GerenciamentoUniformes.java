9f6e2583a133f9896f4474a1fcdf8e30aec6a621
Barbara Mercez <barbaracmercez@gmail.com>
2025-06-06 20:29:20 -0300

    initial files

---
+++ src/main/java/com/mycompany/gerenciamento/uniformes/GerenciamentoUniformes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gerenciamento.uniformes;
+
+/**
+ *
+ * @author barba
+ */
+public class GerenciamentoUniformes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
