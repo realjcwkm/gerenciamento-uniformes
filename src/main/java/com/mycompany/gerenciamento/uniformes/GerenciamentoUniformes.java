f55697a94043401ada77b6167d99157c32858f7c
wagnerelvas1 <wagner.relvas@gmail.com>
2025-06-29 21:42:11 -0400

    refactor: Realoca a função main do arquivo 'ViewsSistema' para 'GerenciamentoUniformes'.

--- src/main/java/com/mycompany/gerenciamento/uniformes/GerenciamentoUniformes.java
+++ src/main/java/com/mycompany/gerenciamento/uniformes/GerenciamentoUniformes.java
@@ -3,20 +3,31 @@
  */
 
 package com.mycompany.gerenciamento.uniformes;
-
-import com.mycompany.gerenciamento.uniformes.DAO.TamanhoDAO;
-import com.mycompany.gerenciamento.uniformes.Models.TamanhoModel;
+import com.mycompany.gerenciamento.uniformes.View.ViewsSistema;
 
 /**
  *
  * @author barba
  */
 public class GerenciamentoUniformes {
-
-    public static void main(String[] args) {
-        TamanhoModel xg = new TamanhoModel();
-        xg.setNome("XG");
         
-        new TamanhoDAO().cadastrarTamanho(xg);
+    public static void main(String[] args) {
+        System.out.println("Método main iniciado!");
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(ViewsSistema.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ViewsSistema().setVisible(true); 
+            }
+        });
     }
 }
