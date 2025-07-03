a7dc8b5aa9dc0a99877b7ab14bca56bc64cd7b42
Robert <robertterto04@gmail.com>
2025-07-03 12:21:22 -0400

    feat:  Cria metodo listar uniforme no UniformeDAO

--- src/main/java/com/mycompany/gerenciamento/uniformes/DAO/UniformeDAO.java
+++ src/main/java/com/mycompany/gerenciamento/uniformes/DAO/UniformeDAO.java
@@ -5,6 +5,8 @@
 package com.mycompany.gerenciamento.uniformes.DAO;
 
 import com.mycompany.gerenciamento.uniformes.DBConnection.Conexao;
+import com.mycompany.gerenciamento.uniformes.Models.TamanhoModel;
+import com.mycompany.gerenciamento.uniformes.Models.TipoUniformeModel;
 import com.mycompany.gerenciamento.uniformes.Models.UniformeModel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -41,4 +43,46 @@ public class UniformeDAO {
             e.printStackTrace();
         }
     }  
+    public List<UniformeModel> listarTodos() {
+        List<UniformeModel> uniformes = new ArrayList<>();
+        String sql = "SELECT " +
+            " u.id AS uniforme_id, " +
+            " u.quantidade AS uniforme_quantidade, " +
+            " t.id AS tamanho_id, " +
+            " t.descricao AS tamanho_descricao, " + 
+            " tp.id AS tipo_id, " +
+            " tp.nome AS tipo_nome " + 
+            "FROM Uniforme AS u " +
+            "JOIN Tamanho AS t ON u.fk_tamanho = t.id " +
+            "JOIN TipoUniforme AS tp ON u.fk_tipo_uniforme = tp.id";
+
+    try (PreparedStatement ps = conn.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery()) {
+
+        while (rs.next()) {
+            UniformeModel uniforme = new UniformeModel();
+            TamanhoModel tamanho = new TamanhoModel();
+            TipoUniformeModel tipoUniforme = new TipoUniformeModel();
+
+            tamanho.setId(rs.getInt("id_tamanho"));
+            tamanho.setDescricao(rs.getString("tamanho")); 
+
+            tipoUniforme.setId(rs.getInt("id_tipo"));
+            tipoUniforme.setNome(rs.getString("tipo"));
+
+            uniforme.setId(rs.getInt("id_uniforme"));
+            uniforme.setQuantidade(rs.getInt("quantidade_uniforme"));
+            uniforme.setTamanho(tamanho); 
+            uniforme.setTipoUniforme(tipoUniforme); 
+            uniformes.add(uniforme);
+        }
+    } catch (SQLException error) {
+        System.err.println("Erro ao listar uniformes: ");
+        error.printStackTrace();
+
+    }
+
+    return uniformes;
+    }
+    
 }
