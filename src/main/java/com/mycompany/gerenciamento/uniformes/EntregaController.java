c929757197dc4dfa0fe1ebce393cf6471d18b818
AnaClaraGibson <anaclarasouzagibson@gmail.com>
2025-07-03 10:37:02 -0400

    feat: Cria o EntregaController e método de listarTodos.

---
+++ src/main/java/com/mycompany/gerenciamento/uniformes/EntregaController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gerenciamento.uniformes;
+
+import com.mycompany.gerenciamento.uniformes.DAO.EntregaDAO;
+import com.mycompany.gerenciamento.uniformes.Models.EntregaModel;
+import java.util.List;
+
+/**
+ *
+ * @author geinfo
+ */
+public class EntregaController {
+    private final EntregaDAO entregaDAO;
+    
+    public EntregaController() {
+        this.entregaDAO = new EntregaDAO();
+    }
+    
+    public List<EntregaModel> listarTodos() {
+        return this.entregaDAO.listarTodos();
+    }
+        
+}
