0df613898cb393a138ece6f417b32cd1c7f4614b
AnaClaraGibson <anaclarasouzagibson@gmail.com>
2025-07-03 10:07:52 -0400

    feat: Modifica o método listarTodos do EntregaDAO para buscar os atributos de seus relacionamentos.

--- src/main/java/com/mycompany/gerenciamento/uniformes/DAO/EntregaDAO.java
+++ src/main/java/com/mycompany/gerenciamento/uniformes/DAO/EntregaDAO.java
@@ -6,7 +6,13 @@ package com.mycompany.gerenciamento.uniformes.DAO;
 
 import com.mycompany.gerenciamento.uniformes.DBConnection.Conexao;
 import com.mycompany.gerenciamento.uniformes.Interfaces.EntregaInterface;
+import com.mycompany.gerenciamento.uniformes.Models.AlunoModel;
 import com.mycompany.gerenciamento.uniformes.Models.EntregaModel;
+import com.mycompany.gerenciamento.uniformes.Models.ServidorModel;
+import com.mycompany.gerenciamento.uniformes.Models.TamanhoModel;
+import com.mycompany.gerenciamento.uniformes.Models.TipoUniformeModel;
+import com.mycompany.gerenciamento.uniformes.Models.TrocaModel;
+import com.mycompany.gerenciamento.uniformes.Models.UniformeModel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -33,28 +39,57 @@ public class EntregaDAO implements EntregaInterface {
             + "u.id AS id_uniforme, u.quantidade AS quantidade_uniforme, "
             + "a.id AS id_aluno, a.matricula AS matricula_aluno, a.nome AS nome_aluno, "
             + "t.id AS id_tamanho, t.nome AS tamanho, "
-            + "tu.id AS id_tipo, tu.nome AS tipo "
+            + "tu.id AS id_tipo, tu.nome AS tipo,"
+            + "tr.id AS id_troca, tr.data_troca AS data_troca "
             + "FROM Entrega AS e "
             + "LEFT JOIN Servidor AS s ON e.fk_servidor = s.id "
             + "LEFT JOIN Uniforme AS u ON e.fk_uniforme = u.id "
             + "LEFT JOIN Aluno AS a ON e.fk_aluno = a.id "
             + "LEFT JOIN Tamanho AS t ON u.fk_tamanho = t.id "
-            + "LEFT JOIN TipoUniforme AS tu ON u.fk_tipo_uniforme = tu.id";
+            + "LEFT JOIN TipoUniforme AS tu ON u.fk_tipo_uniforme = tu.id "
+            + "LEFT JOIN Troca AS tr ON tr.fk_entrega_antiga = e.id";
     
     try (PreparedStatement ps = conn.prepareStatement(sql); 
          ResultSet rs = ps.executeQuery()) {
         
         while(rs.next()) {
             EntregaModel entrega = new EntregaModel();
+            ServidorModel servidor = new ServidorModel();
+            UniformeModel uniforme = new UniformeModel();
+            AlunoModel aluno = new AlunoModel();
+            TamanhoModel tamanho = new TamanhoModel();
+            TipoUniformeModel tipoUniforme = new TipoUniformeModel();
+//            TrocaModel troca = new TrocaModel();
+            
             entrega.setId(rs.getInt("id"));
             entrega.setSemestre(rs.getInt("semestre"));
             entrega.setAno(rs.getInt("ano"));
             entrega.setData_entrega(rs.getDate("data_entrega").toLocalDate());
             entrega.setTrocado(rs.getBoolean("trocado"));
             entrega.setQuantidade(rs.getInt("quantidade"));
-            entrega.setFk_servidor(rs.getInt("fk_servidor"));
-            entrega.setFk_aluno(rs.getInt("fk_aluno"));
-            entrega.setFk_uniforme(rs.getInt("fk_uniforme"));
+            
+            servidor.setId(rs.getInt("id_servidor"));
+            servidor.setNome(rs.getString("nome_servidor"));
+            
+            aluno.setId(rs.getInt("id_aluno"));
+            aluno.setNome(rs.getString("nome_aluno"));
+            aluno.setMatricula(rs.getString("matricula_aluno"));
+            
+            tamanho.setId(rs.getInt("id_tamanho"));
+            tamanho.setNome(rs.getString("tamanho"));
+            
+            tipoUniforme.setId(rs.getInt("id_tipo"));
+            tipoUniforme.setNome(rs.getString("tipo"));
+//            troca.setData_troca(rs.getDate("data_troca").toLocalDate());
+
+            uniforme.setId(rs.getInt("id_uniforme"));
+            uniforme.setTamanho(tamanho); 
+            uniforme.setTipoUniforme(tipoUniforme);
+            
+            
+            entrega.setServidor(servidor);
+            entrega.setUniforme(uniforme);
+            entrega.setAluno(aluno);
             
             entregas.add(entrega);
         }
