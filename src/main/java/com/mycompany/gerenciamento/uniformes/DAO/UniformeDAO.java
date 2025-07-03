16575c8d7a1f34da44ba5bfd1082297b7c41600d
Robert <robertterto04@gmail.com>
2025-07-03 11:27:38 -0400

    feat: Cria DAO de Uniforme

---
+++ src/main/java/com/mycompany/gerenciamento/uniformes/DAO/UniformeDAO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gerenciamento.uniformes.DAO;
+
+import com.mycompany.gerenciamento.uniformes.DBConnection.Conexao;
+import com.mycompany.gerenciamento.uniformes.Models.UniformeModel;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rober
+ */
+public class UniformeDAO {
+    private Connection conn;
+    
+    public UniformeDAO(){
+    this.conn = Conexao.getConexao();
+    }  
+}
