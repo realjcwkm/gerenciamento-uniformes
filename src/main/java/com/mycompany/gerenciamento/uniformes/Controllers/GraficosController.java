29dc47c34c83635b080452886baba7a0ea605f0f
Barbara Mercez <barbaracmercez@gmail.com>
2025-07-04 20:52:33 -0300

    refactor: Realoca as controllers para o pacote 'Controllers'.

--- src/main/java/com/mycompany/gerenciamento/uniformes/Controllers/GraficosController.java
+++ src/main/java/com/mycompany/gerenciamento/uniformes/Controllers/GraficosController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.gerenciamento.uniformes;
+package com.mycompany.gerenciamento.uniformes.Controllers;
 
 import com.mycompany.gerenciamento.uniformes.DAO.EntregaDAO; // Importe seu DAO
 import java.awt.Color;
