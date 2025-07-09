f3d31e3e222fc5b96fd3c5db8434d1cceaf422cc
wagnerelvas1 <wagner.relvas@gmail.com>
2025-07-09 18:39:13 -0400

    feat: Cria a função de cadastro de alunos

--- src/main/java/com/mycompany/gerenciamento/uniformes/DAO/AlunoDAO.java
+++ src/main/java/com/mycompany/gerenciamento/uniformes/DAO/AlunoDAO.java
@@ -12,6 +12,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -98,5 +99,39 @@ public class AlunoDAO implements AlunoInterface {
         }
         
         return null;
+    }
+    
+    @Override
+    public boolean cadastrar(AlunoModel aluno) {
+        String sql = "INSERT INTO Aluno (nome, sobrenome, email, telefone, matricula, idade, periodo, fk_curso) "
+                   + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        try (PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, aluno.getNome());
+            ps.setString(2, aluno.getSobrenome());
+            ps.setString(3, aluno.getEmail());
+            ps.setString(4, aluno.getTelefone());
+            ps.setString(5, aluno.getMatricula());
+            ps.setInt(6, aluno.getIdade());
+            ps.setInt(7, aluno.getPeriodo());
+            ps.setInt(8, aluno.getFk_curso());
+            
+            int linhasAfetadas = ps.executeUpdate(sql);
+            
+            if (linhasAfetadas == 1) {
+                try (ResultSet rs = ps.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        aluno.setId(rs.getInt(1));
+                        System.out.println("Aluno Cadastrado com ID: " + aluno.getId());
+                        return true;
+                    }
+                }
+            }
+            return true;
+        } catch (SQLException e) {
+            System.err.println("Erro ao cadastrar aluno:");
+            e.printStackTrace();
+            return false;
+        }
     }
 }
