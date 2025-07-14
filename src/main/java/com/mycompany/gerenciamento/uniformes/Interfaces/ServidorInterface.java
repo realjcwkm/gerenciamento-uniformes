bcdf06df9ee19f7f079b76db442d2ac74564f74c
BarbaraMercez <barbaracmercez@gmail.com>
2025-07-14 16:56:12 -0400

    feat: Adiciona os métodos getTotal e listaPagina em ServidorInterface.

--- src/main/java/com/mycompany/gerenciamento/uniformes/Interfaces/ServidorInterface.java
+++ src/main/java/com/mycompany/gerenciamento/uniformes/Interfaces/ServidorInterface.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.gerenciamento.uniformes.Interfaces;
 
+import com.mycompany.gerenciamento.uniformes.Models.FiltroModel;
 import com.mycompany.gerenciamento.uniformes.Models.ServidorModel;
 import java.sql.SQLException;
 import java.util.List;
@@ -13,11 +14,12 @@ import java.util.List;
  * @author w
  */
 public interface ServidorInterface {
-    public List<ServidorModel> listarTodos();
     public ServidorModel getByMatricula(String matricula);
     public boolean cadastrar(ServidorModel servidor);
     public boolean updateSenha(String matricula, String senha);
     public boolean verificarDepartamento(int idDepartamento) throws SQLException;
     void editar(ServidorModel servidor);
     public boolean excluir(int id);
+    List<ServidorModel> listarPagina(int pagina, int itensPorPagina, String termoBusca, FiltroModel filtroDepto, FiltroModel filtroStatus);
+    int getTotal(String termoBusca, FiltroModel filtroDepto, FiltroModel filtroStatus);
 }
